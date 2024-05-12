a5e0c1c5090c2d4cdb7c77f6cabb43a9cbb85ebe
Vishnu372 <134490897+Vishnu372@users.noreply.github.com>
2024-05-12 15:42:09 +0530

    Time: 0 ms (100%), Space: 44.4 MB (45.79%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,20 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false; // If the list is empty or has only one node, there can't be a cycle
+        if(head==null||head.next==null)
+        {
+            return false;
         }
-        
-        ListNode slow = head;
-        ListNode fast = head.next; // Fast pointer moves two steps at a time initially
-        
-        while (fast != null && fast.next != null) {
-            if (slow == fast) {
-                return true; // If slow and fast pointers meet, there's a cycle
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null&&fast.next!=null)
+        {
+            if(slow==fast)
+            {
+                return true;
             }
-            slow = slow.next;
-            fast = fast.next.next;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        
-        return false; // If fast pointer reaches the end, there's no cycle
+        return false;
     }
 }
