e43d50699adb0f4912d8534942b6ac1f193f6c31
Vishnu372 <134490897+Vishnu372@users.noreply.github.com>
2024-05-15 07:37:16 +0530

    Time: 0 ms (100%), Space: 40 MB (83.93%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        int x=n%2;
+        if(x==1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
