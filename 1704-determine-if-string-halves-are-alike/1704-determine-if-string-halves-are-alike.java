fd32c4efd6e2d30ea11560113b5954a50c1418db
Vishnu372 <134490897+Vishnu372@users.noreply.github.com>
2024-06-17 17:03:58 +0530

    Time: 9 ms (30.50%), Space: 41.6 MB (66.32%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        s = s.toLowerCase(); // Reassign to make s lowercase
-        int n = s.length();
-        int c1 = 0, c2 = 0;
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+        s=s.toLowerCase();
+        int n=s.length();
+        int c1=0,c2=0;
+        for(int i=0;i<n/2;i++)
+        {
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+            {
                 c1++;
             }
         }
-        for (int i = n / 2; i < n; i++) {
-            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+        for(int i=n/2;i<n;i++)
+        {
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+            {
                 c2++;
             }
         }
-        return c1 == c2;
+        return c1==c2;
     }
 }
